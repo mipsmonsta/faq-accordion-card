6e8af07cc0bea033666182e806938d899deeebe8
mipsmonsta <yaojintham@gmail.com>
2021-06-04 22:58:55 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let questions = document.getElementsByClassName("question");
+
+let i;
+for (i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    let answer = this.nextElementSibling;
+    let imgElement = this.firstElementChild;
+    if (answer.style.display == "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+
+    if (this.classList.contains("active")) {
+      imgElement.style.transform = "rotate(180deg)";
+    } else {
+      imgElement.style.transform = "rotate(0deg)";
+    }
+  });
+}
